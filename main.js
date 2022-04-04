9ee812d43b405a6acb31e4c3da14530205799934
Whitepotter <89248447+Whitepotter@users.noreply.github.com>
2022-04-04 10:25:36 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function redirect1(){
+    window.location = "second_page.html";
+}
+
+function redirect2(){
+    window.location = "dognose.html";
+}
+
+function redirect3(){
+    window.location = "mustache.html";
+}
---
+++ mustache.js
@@ -0,0 +1,43 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+    img = loadImage("https://i.postimg.cc/qBjggMjx/moustache-png-image-mustache-png-2000-1021.png");
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+function draw(){
+    image(video,0,0,300,300);
+    image(img,noseX-15,noseY,30,30);
+}
+
+function take_snapshot(){
+    save("my_filter_image.png");
+}
+
+function homescreen_3(){
+    window.location = "index.html";
+}
